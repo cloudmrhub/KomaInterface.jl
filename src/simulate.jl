a2697f29452bb0fa253ff94365561846f428ce73
Eros <eros.montin@gmail.com>
2026-08-06 15:59:38 -0400

    fix: use 7-arg Grad constructor for KomaMRI 0.10.4 compatibility

--- src/simulate.jl
+++ src/simulate.jl
@@ -42,7 +42,7 @@ function rescale_fov!(seq::Sequence, scale::Vararg{Float64,N}) where {N}
     for j in axes(seq.GR,2),i in 1:N
         g = seq.GR[i,j]
         all(iszero,g.A) && continue
-        seq.GR[i,j] = typeof(g)(g.A .* scale[i], g.T, g.rise, g.fall, g.delay)
+        seq.GR[i,j] = Grad(g.A .* scale[i], g.T, g.rise, g.fall, g.delay, g.first, g.last)
     end
 end
 
